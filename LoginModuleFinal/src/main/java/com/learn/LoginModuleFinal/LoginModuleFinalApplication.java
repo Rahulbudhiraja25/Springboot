bb56cb7b377d3df291e18910f8e77407d74c091a
RahulBudhiraja25 <budhirajarahul25@gmail.com>
2024-08-17 08:35:51 +0530

    Starting a login module project that can be intgrated further with any other projects

---
+++ LoginModuleFinal/src/main/java/com/learn/LoginModuleFinal/LoginModuleFinalApplication.java
@@ -0,0 +1,13 @@
+package com.learn.LoginModuleFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginModuleFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginModuleFinalApplication.class, args);
+	}
+
+}
---
+++ LoginModuleFinal/src/test/java/com/learn/LoginModuleFinal/LoginModuleFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.LoginModuleFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginModuleFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
